2728c291a064e3bba41ab8558da31130927d8b28
Hemalatha Arul <hemalathaa@infinite.com>
2024-03-11 15:37:35 +0530

    assignment_4

---
+++ csharp/Assignment/Assignment_4/Assignment_4/Assignment_4/Letter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Letter
+
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter words :");
+        string input = Console.ReadLine();
+        List<string> words = input.Split(' ').ToList();
+
+
+        var result = words.Where(word => word.StartsWith("a", StringComparison.OrdinalIgnoreCase) && word.EndsWith("m", StringComparison.OrdinalIgnoreCase));
+
+        if (result.Count() > 2 || result.Count() > 3)
+        {
+            Console.WriteLine("Words starting with 'a' and ending with 'm':");
+            foreach (var word in result)
+            {
+                Console.WriteLine(word);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No words are starting with 'a' and ending with 'm'.");
+        }
+
+        Console.Read();
+    }
+}
