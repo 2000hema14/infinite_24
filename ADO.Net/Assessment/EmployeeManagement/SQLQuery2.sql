1b731ffde7814bccf88686cb3516cb7897b3ceac
Hemalatha Arul <hemalathaa@infinite.com>
2024-04-12 11:43:05 +0530

    Assesment

--- ADO.Net/Assessment/EmployeeManagement/SQLQuery2.sql
+++ ADO.Net/Assessment/EmployeeManagement/SQLQuery2.sql
@@ -7,6 +7,7 @@ create table  Employee_Details (
     Empsal NUMERIC(10,2) CHECK (Empsal >= 25000),
     Emptype CHAR(1) CHECK (Emptype IN ('P', 'C'))
 );
+SELECT *FROM  Employee_Details
 
 -----PROCEDURE--------
 
@@ -27,20 +28,31 @@ END
 
 -----OUTPUT-------
 
-Enter Employee_name:
-Tarun
-Enter Employee_Salary:
-55000
-Enter employee type ( Permanent (P),Contract(C)):
-c
-Employee Management details added successfully!
-All Employee Rows:
-Empno: 1, EmpName: Varun, Empsal: 25000.00, Emptype: C
-Empno: 2, EmpName: HEMA, Empsal: 75000.00, Emptype: P
-Empno: 3, EmpName: RAGHAVA, Empsal: 85000.00, Emptype: P
-Empno: 4, EmpName: RAMYA SHREE, Empsal: 65000.00, Emptype: C
-Empno: 5, EmpName: gowtham, Empsal: 60000.00, Emptype: C
-Empno: 6, EmpName: Tarun, Empsal: 55000.00, Emptype: c
+--Enter Employee_name:
+--Tarun
+--Enter Employee_Salary:
+--55000
+--Enter employee type ( Permanent (P),Contract(C)):
+--c
+--Employee Management details added successfully!
+--All Employee Rows:
+--Empno: 1, EmpName: Varun, Empsal: 25000.00, Emptype: C
+--Empno: 2, EmpName: HEMA, Empsal: 75000.00, Emptype: P
+--Empno: 3, EmpName: RAGHAVA, Empsal: 85000.00, Emptype: P
+--Empno: 4, EmpName: RAMYA SHREE, Empsal: 65000.00, Emptype: C
+--Empno: 5, EmpName: gowtham, Empsal: 60000.00, Emptype: C
+--Empno: 6, EmpName: Tarun, Empsal: 55000.00, Emptype: c
+
+---------EXCEPTION CASE--------------
+
+----Enter Employee_name:
+----akash
+----Enter Employee_Salary:
+----20000
+----Enter employee type ( Permanent (P),Contract(C)):
+----p
+----Error: The INSERT statement conflicted with the CHECK constraint "CK__Employee___Empsa__37A5467C". The conflict occurred in database "EmployeeManagementDB", table "dbo.Employee_Details", column 'Empsal'.
+----The statement has been terminated.
 
 
 
